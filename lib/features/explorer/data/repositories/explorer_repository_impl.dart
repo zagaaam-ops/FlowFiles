063e490b0bf331b41683d0784b1c3cc255941334
zagaaam-ops <zagaaam@gmail.com>
2026-07-26 15:41:22 +0300

    Update explorer_repository_impl.dart

--- lib/features/explorer/data/repositories/explorer_repository_impl.dart
+++ lib/features/explorer/data/repositories/explorer_repository_impl.dart
@@ -13,4 +13,24 @@ Future<void> moveFiles({
 
     await source.rename(destination);
   }
+@override
+Future<void> moveFiles({
+  required List<String> sourcePaths,
+  required String destinationPath,
+}) async {
+  for (final sourcePath in sourcePaths) {
+    final source = File(sourcePath);
+
+    if (!await source.exists()) {
+      continue;
+    }
+
+    final destination = p.join(
+      destinationPath,
+      p.basename(sourcePath),
+    );
+
+    await source.rename(destination);
+  }
+}
 }
