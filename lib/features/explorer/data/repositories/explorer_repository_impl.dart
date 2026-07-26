c6fcf4dfeea20adc992edf38714f1c8dd76f62f4
zagaaam-ops <zagaaam@gmail.com>
2026-07-26 16:03:34 +0300

    Create explorer_repository_impl.dart

---
+++ lib/features/explorer/data/repositories/explorer_repository_impl.dart
@@ -0,0 +1,26 @@
+@override
+Future<DirectoryEntity> refreshDirectory(String path) {
+  return loadDirectory(path);
+}
+
+@override
+Future<void> moveFiles({
+  required List<String> sourcePaths,
+  required String destinationPath,
+}) async {
+  for (final sourcePath in sourcePaths) {
+    final source = File(sourcePath);
+
+    if (!await source.exists()) {
+      continue;
+    }
+
+    final destination = p.join(
+      destinationPath,
+      p.basename(sourcePath),
+    );
+
+    await source.rename(destination);
+  }
+}
+}
