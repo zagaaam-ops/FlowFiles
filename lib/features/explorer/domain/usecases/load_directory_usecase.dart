48b9cfa7141142b9dcef8a9dc577efab94047a97
zagaaam-ops <zagaaam@gmail.com>
2026-07-26 12:03:38 +0300

    Update load_directory_usecase.dart
    Commit 008A - Add LoadDirectoryUseCase

--- lib/features/explorer/domain/usecases/load_directory_usecase.dart
+++ lib/features/explorer/domain/usecases/load_directory_usecase.dart
@@ -1,16 +1,24 @@
-import '../entities/directory_content.dart';
+import '../entities/directory_entity.dart';
 import '../repositories/explorer_repository.dart';
 
+/// Loads a directory from the repository.
+///
+/// This use case represents the primary business action of the
+/// Explorer Engine.
+///
+/// Additional business rules such as:
+/// - permission checking
+/// - path validation
+/// - logging
+/// - analytics
+/// can be added here without affecting the UI.
 class LoadDirectoryUseCase {
-  const LoadDirectoryUseCase(
-    this.repository,
-  );
+  const LoadDirectoryUseCase(this._repository);
 
-  final ExplorerRepository repository;
+  final ExplorerRepository _repository;
 
-  Future<DirectoryContent> call(
-    String path,
-  ) {
-    return repository.loadDirectory(path);
+  /// Loads the directory located at [path].
+  Future<DirectoryEntity> call(String path) {
+    return _repository.loadDirectory(path);
   }
 }
