ab627e2f4a502cf78eac41aa013888df7bafbe55
zagaaam-ops <zagaaam@gmail.com>
2026-07-26 16:26:14 +0300

    Update explorer_state.dart

--- lib/features/explorer/presentation/state/explorer_state.dart
+++ lib/features/explorer/presentation/state/explorer_state.dart
@@ -1,37 +1,25 @@
 import '../../domain/entities/directory_entity.dart';
 
-enum ExplorerStatus {
-  initial,
-  loading,
-  loaded,
-  empty,
-  error,
-}
-
 class ExplorerState {
   const ExplorerState({
-    this.status = ExplorerStatus.initial,
-    this.currentPath = '',
-    this.content,
+    this.directory,
+    this.isLoading = false,
     this.errorMessage,
   });
 
-  final ExplorerStatus status;
-  final String currentPath;
-  final DirectoryEntity? content;
+  final DirectoryEntity? directory;
+  final bool isLoading;
   final String? errorMessage;
 
   ExplorerState copyWith({
-    ExplorerStatus? status,
-    String? currentPath,
-    DirectoryEntity? content,
+    DirectoryEntity? directory,
+    bool? isLoading,
     String? errorMessage,
   }) {
     return ExplorerState(
-      status: status ?? this.status,
-      currentPath: currentPath ?? this.currentPath,
-      content: content ?? this.content,
-      errorMessage: errorMessage ?? this.errorMessage,
+      directory: directory ?? this.directory,
+      isLoading: isLoading ?? this.isLoading,
+      errorMessage: errorMessage,
     );
   }
 }
