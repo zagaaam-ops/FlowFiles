91b6421b2d8af8239faa2063770d00cfdf9bef0b
zagaaam-ops <zagaaam@gmail.com>
2026-07-26 16:30:01 +0300

    Update explorer_provider.dart

--- lib/features/explorer/presentation/providers/explorer_provider.dart
+++ lib/features/explorer/presentation/providers/explorer_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../data/datasources/file_system_datasource.dart';
+import '../../data/datasources/local_file_system_data_source.dart';
 import '../../data/repositories/explorer_repository_impl.dart';
 import '../../domain/usecases/load_directory_usecase.dart';
 import 'explorer_notifier.dart';
@@ -10,8 +10,7 @@ final explorerProvider =
     StateNotifierProvider<
         ExplorerNotifier,
         ExplorerState>((ref) {
-  final repository = ExplorerRepositoryImpl(
-    const FileSystemDataSource(),
+  final repository = ExplorerRepositoryImpl(LocalFileSystemDataSource())
   );
 
   return ExplorerNotifier(
