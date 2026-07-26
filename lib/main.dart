d13c5cbc1bae6d0c882482d1359321cf918a1e33
zagaaam-ops <zagaaam@gmail.com>
2026-07-26 12:41:39 +0300

    Commit 010D - Connect ExplorerPage as application home

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'app/app.dart';
+import 'features/explorer/presentation/pages/explorer_page.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const FlowFilesApp());
+}
 
-  runApp(
-    const ProviderScope(
-      child: FastFileOrganizerApp(),
-    ),
-  );
+class FlowFilesApp extends StatelessWidget {
+  const FlowFilesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlowFiles',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const ExplorerPage(),
+    );
+  }
 }
