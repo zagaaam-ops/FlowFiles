fcd873ef3c36fb8a06692e6b19c43a9bdb022b9c
zagaaam-ops <zagaaam@gmail.com>
2026-07-28 10:05:28 +0300

    Update explorer_page.dart

--- lib/features/explorer/presentation/pages/explorer_page.dart
+++ lib/features/explorer/presentation/pages/explorer_page.dart
@@ -180,10 +180,8 @@ class _ExplorerPageState extends State<ExplorerPage> {
                         }
 
                         return FileTile(
-                          file: item,
-                          selected: selectionController.isSelected(item.path),
-                          onTap: () {
-                            _handleItemTap(item.path);
+                     _handleItemTap(item.path);
+},
                           },
                         );
                       },
