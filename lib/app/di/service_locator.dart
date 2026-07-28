7eeffa132efeb2dea7b05aa35955542a27aae9a2
zagaaam-ops <zagaaam@gmail.com>
2026-07-28 12:04:49 +0000

    checkpoint: register clipboard controller

--- lib/app/di/service_locator.dart
+++ lib/app/di/service_locator.dart
@@ -1,13 +1,13 @@
 import '../../features/explorer/data/datasources/local_file_system_data_source.dart';
 import '../../features/explorer/data/repositories/explorer_repository_impl.dart';
 import '../../features/explorer/domain/usecases/load_directory_usecase.dart';
+import '../../features/explorer/presentation/controllers/clipboard_controller.dart';
 import '../../features/explorer/presentation/controllers/explorer_controller.dart';
 import '../../features/explorer/presentation/controllers/selection_controller.dart';
 
 /// Simple dependency container for FlowFiles.
 ///
-/// Later this can be replaced by GetIt or Riverpod,
-/// but for now it keeps object creation centralized.
+/// Later this can be replaced by GetIt or Riverpod.
 class ServiceLocator {
   ServiceLocator._();
 
@@ -23,6 +23,7 @@ class ServiceLocator {
   static final ExplorerController explorerController =
       ExplorerController(_loadDirectoryUseCase);
 
-  static final SelectionController selectionController =
-      SelectionController();
+  static final SelectionController selectionController = SelectionController();
+
+  static final ClipboardController clipboardController = ClipboardController();
 }
